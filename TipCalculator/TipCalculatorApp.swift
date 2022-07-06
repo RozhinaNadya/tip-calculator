75267f9410161024be1a592abff455d0721c311a
RozhinaNadya <zhuk.09@mail.ru>
2022-07-06 15:25:04 +0200

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Надежда on 2022-07-06.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
